786d39c9a084882e22501f002fd27d4a1c85cad4
Nuomisu <nuonuomisu@gmail.com>
2015-01-06 21:38:13 +0800

    add more feature

--- js/models/article.js
+++ js/models/article.js
@@ -1,19 +1,30 @@
 Blog.Article = DS.Model.extend({
   title: DS.attr('string'),
-  isCompleted: DS.attr('boolean')
+  date: DS.attr('date'),
+  introduction: DS.attr('string'),
+  content: DS.attr('string')
 });
 
 Blog.Article.FIXTURES = [
  {
    id: 1,
-   title: 'Learn Ember.js',
+   title: 'blog One',
+   date: new Date("2013-9-30"),
+   introduction: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+   content: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
  },
  {
    id: 2,
-   title: '...',
+   title: 'blog Two',
+   date: new Date("2014-10-04"),
+   introduction: 'Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.',
+   content: 'Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.'
  },
  {
    id: 3,
-   title: 'Profit!',
+   title: 'blog Three',
+   date: new Date(),
+   introduction: 'Maecenas sed diam eget risus varius blandit sit amet non magna.',
+   content: 'Maecenas sed diam eget risus varius blandit sit amet non magna.'
  }
 ];
