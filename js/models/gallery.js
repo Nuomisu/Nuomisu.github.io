89ca9a09ac3d749cc7b1de68883c783d6709181e
Nuomisu <nuonuomisu@gmail.com>
2015-01-12 17:12:46 +0800

    add image

--- js/models/gallery.js
+++ js/models/gallery.js
@@ -10,8 +10,8 @@ MyApp.Gallery.FIXTURES = [
 id: 1,
 name: 'City Gate Tower',
 description: 'One city gate Tower after a battle.',
-link1:'img/gallery/chenglou.png',
-link2:'img/gallery/chenglou_s.png'
+link1:'img/gallery/chenglou.jpg',
+link2:'img/gallery/chenglou_s.jpg'
 },
 {
 id: 2,
@@ -24,42 +24,42 @@ link2:'img/gallery/moon_s.jpg'
 id: 3,
 name: 'Princess',
 description: 'A pink princess',
-link1:'img/gallery/princess.png',
-link2:'img/gallery/princess.png'
+link1:'img/gallery/princess.jpg',
+link2:'img/gallery/princess_s.jpg'
 },
 {
 id: 4,
 name: 'Flowers',
 description: 'One background of this website',
-link1:'img/gallery/flower.png',
-link2:'img/gallery/flower.png'
+link1:'img/gallery/flower.jpg',
+link2:'img/gallery/flower_s.jpg'
 },
 {
 id: 5,
-name: 'Sword',
-description: 'A copied line draft of one sword',
-link1:'img/gallery/dao.png',
-link2:'img/gallery/dao_s.png'
+name: 'Rose',
+description: 'My rose',
+link1:'img/gallery/rose.jpg',
+link2:'img/gallery/rose_s.jpg'
 },
 {
 id: 6,
+name: 'A Ruan',
+description: 'Main Character of Gujianqitan',
+link1:'img/gallery/ruan.jpg',
+link2:'img/gallery/ruan_s.jpg'
+},
+{
+id: 7,
+name: 'Sword',
+description: 'A copied line draft of one sword',
+link1:'img/gallery/dao.jpg',
+link2:'img/gallery/dao_s.jpg'
+},
+{
+id: 8,
 name: 'Chingay-side view',
 description: 'A side-view design of Chingay float',
 link1:'img/gallery/chingay.jpg',
 link2:'img/gallery/chingay_s.jpg'
-},
-{
-id: 7,
-name: 'Image Seven',
-description: 'Seven Image',
-link1:'img/gallery/flower.png',
-link2:'img/gallery/flower.png'
-},
-{
-id: 8,
-name: 'Image Eight',
-description: 'Eight Image',
-link1:'img/gallery/flower.png',
-link2:'img/gallery/flower.png'
 }
 ];
